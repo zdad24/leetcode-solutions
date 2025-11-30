b5b85651da871b5455948040a88865b6d5186c0b
Zahadad Jarif <zahadad14@gmail.com>
2025-11-30 17:39:26 -0500

    File updated at 11/30/2025, 5:39:26 PM

--- 49-GroupAnagrams/49-GroupAnagrams.py
+++ 49-GroupAnagrams/49-GroupAnagrams.py
@@ -1,16 +1,16 @@
-# Last updated: 11/30/2025, 3:39:40 PM
-class Solution(object):
-    def groupAnagrams(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
-        result = defaultdict(list)
-
-        for s in strs:
-            count = [0] * 26
-            for c in s:
-                count[ord(c) - ord("a")] +=1
-            result[tuple(count)].append(s)
-
-        return result.values()
+# Last updated: 11/30/2025, 5:39:26 PM
+1class Solution(object):
+2    def groupAnagrams(self, strs):
+3        """
+4        :type strs: List[str]
+5        :rtype: List[List[str]]
+6        """
+7        result = defaultdict(list)
+8
+9        for s in strs:
+10            count = [0] * 26
+11            for c in s:
+12                count[ord(c) - ord("a")] +=1
+13            result[tuple(count)].append(s)
+14
+15        return result.values()
