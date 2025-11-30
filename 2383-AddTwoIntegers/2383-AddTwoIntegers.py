3a92259f50f5f973fd1492878d995aba916b991c
Zahadad Jarif <zahadad14@gmail.com>
2025-11-30 15:39:38 -0500

    Create 2383-AddTwoIntegers

---
+++ 2383-AddTwoIntegers/2383-AddTwoIntegers.py
@@ -0,0 +1,10 @@
+# Last updated: 11/30/2025, 3:39:37 PM
+class Solution(object):
+    def sum(self, num1, num2):
+        """
+        :type num1: int
+        :type num2: int
+        :rtype: int
+        """
+        return num1 + num2
+        
