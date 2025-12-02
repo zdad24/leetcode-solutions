14324a0f025e990128fcf69e020b430bd6e2e60d
Zahadad Jarif <zahadad14@gmail.com>
2025-12-02 13:54:42 -0500

    File updated at 12/2/2025, 1:54:41 PM

--- 9-PalindromeNumber/9-PalindromeNumber.py
+++ 9-PalindromeNumber/9-PalindromeNumber.py
@@ -1,7 +1,6 @@
-# Last updated: 12/2/2025, 1:54:36 PM
+# Last updated: 12/2/2025, 1:54:41 PM
 1class Solution:
 2    def isPalindrome(self, x: int) -> bool:
 3        strx = str(x)
 4        strrev = strx[::-1]
-5
-6        return strx == strrev
+5        return strx == strrev
