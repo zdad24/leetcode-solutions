ad35cccb5d9f72e1f2ba87147836c79c731698a7
Zahadad Jarif <zahadad14@gmail.com>
2025-12-18 14:29:21 -0500

    File updated at 12/18/2025, 2:29:21 PM

--- 217-ContainsDuplicate/217-ContainsDuplicate.py
+++ 217-ContainsDuplicate/217-ContainsDuplicate.py
@@ -1,15 +1,16 @@
-# Last updated: 11/30/2025, 3:39:39 PM
-class Solution(object):
-    def containsDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
-
-        hashset = set()
-        
-        for num in nums:
-            if num in hashset:
-                return True
-            hashset.add(num)
-        return False
+# Last updated: 12/18/2025, 2:29:21 PM
+1class Solution(object):
+2    def containsDuplicate(self, nums):
+3        """
+4        :type nums: List[int]
+5        :rtype: bool
+6        """
+7        seen = set()
+8
+9        for num in nums:
+10            if num in seen:
+11                return True
+12            else:
+13                seen.add(num)
+14        
+15        return False
