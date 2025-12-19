bd2f73dc746f0ceb3bc16a9bff6503b6a352c966
Zahadad Jarif <zahadad14@gmail.com>
2025-12-19 14:52:01 -0500

    File updated at 12/19/2025, 2:52:01 PM

--- 242-ValidAnagram/242-ValidAnagram.py
+++ 242-ValidAnagram/242-ValidAnagram.py
@@ -1,16 +1,19 @@
-# Last updated: 11/30/2025, 3:39:36 PM
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        s = sorted(s)
-        t = sorted(t)
-
-        if (s == t):
-            return True
-        return False
-
-        
+# Last updated: 12/19/2025, 2:52:01 PM
+1class Solution:
+2    def isAnagram(self, s: str, t: str) -> bool:
+3        chars1 = dict()
+4        chars2 = dict()
+5        for c in s:
+6            if c in chars1:
+7                chars1[c] +=1
+8            else:
+9                chars1[c] = 1
+10
+11        for c in t:
+12            if c in chars2:
+13                chars2[c] +=1
+14            else:
+15                chars2[c] = 1
+16        
+17        return chars1 == chars2
+18        
