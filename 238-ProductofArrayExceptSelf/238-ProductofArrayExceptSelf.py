1382e9d2425730bdcae5d5f1222feb621eab38bf
Zahadad Jarif <zahadad14@gmail.com>
2025-12-22 19:43:14 -0500

    File updated at 12/22/2025, 7:43:14 PM

--- 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.py
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.py
@@ -1,34 +1,16 @@
-# Last updated: 12/11/2025, 5:31:35 PM
+# Last updated: 12/22/2025, 7:43:14 PM
 1class Solution:
-2    def productExceptSelf(self, nums: List[int]) -> List[int]:
-3        result = []
-4        
-5        prefix = []
+2    def lengthOfLongestSubstring(self, s: str) -> int:
+3        seen = set()
+4        res = 0
+5        l = 0
 6
-7        postfix = []
-8        ans = 1
-9        for i in range(len(nums)):
-10            ans *= nums[i]
-11            prefix.append(ans)
-12
-13        ans = 1
-14        for i in range(len(nums)-1, -1, -1):
-15            ans *= nums[i]
-16            postfix.append(ans)
-17
-18        postfix = postfix[::-1]
-19
-20        for i in range(len(nums)):
-21            if i == 0:
-22                ans = 1 * postfix[i+1]
-23                result.append(ans)
-24            elif i == len(nums)-1:
-25                ans = prefix[i-1] * 1
-26                result.append(ans)
-27            else:
-28                ans = prefix[i-1] * postfix[i+1]
-29                result.append(ans)
-30        return result
-31
-32
-33 
+7        for r in range(len(s)):
+8            while s[r] in seen:
+9                seen.remove(s[l])
+10                l+=1
+11            seen.add(s[r])
+12            res = max(res, r - l +1)
+13
+14        return res
+15
