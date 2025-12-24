04c784b08d6c47a4bd80225a2ca3848ba2d17650
Zahadad Jarif <zahadad14@gmail.com>
2025-12-24 17:59:54 -0500

    File updated at 12/24/2025, 5:59:53 PM

--- 347-TopKFrequentElements/347-TopKFrequentElements.py
+++ 347-TopKFrequentElements/347-TopKFrequentElements.py
@@ -1,31 +1,22 @@
-# Last updated: 12/1/2025, 8:26:01 AM
-1class Solution(object):
-2    def topKFrequent(self, nums, k):
-3        """
-4        :type nums: List[int]
-5        :type k: int
-6        :rtype: List[int]
-7        """
-8
-9        count = defaultdict(int)
-10        answer = []
-11
-12        #count nums
-13        for num in nums:
-14            count[num] += 1
-15        
-16       #sort and switch 
-17        freqlist = []
-18        for key, value in count.items():
-19            freqlist.append((value, key))
-20        
-21        freqlist.sort(reverse = True)
-22
-23        for key, value in freqlist:
-24            if (k == 0):
-25                break
-26            answer.append(value)
-27            k-=1
-28            
-29            
-30        return answer
+# Last updated: 12/24/2025, 5:59:53 PM
+1class Solution:
+2    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+3        count = defaultdict(int)
+4        freq = []
+5        res = []
+6
+7        for num in nums:
+8            count[num] = 1 + count.get(num, 0)
+9
+10        for key, value in count.items():
+11            freq.append((value, key))
+12
+13        freq.sort(reverse = True)
+14        for key, value in freq:
+15            if k == 0:
+16                break
+17            res.append(value)
+18            k-=1
+19    
+20        return res
+21
